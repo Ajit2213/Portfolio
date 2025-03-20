d1458d3dc376acae2bf95d093bc4a099f431a2ce
ajit 2213 <ajitofficial7070@gmail.com>
2025-03-20 19:10:19 +0530

    done

--- Portfolio/src/component/Hero.jsx
+++ Portfolio/src/component/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import resume from "../assets/resume.pdf"
+import resume1 from "../assets/AjitResume.pdf.pdf"
 import ajit from "../assets/ajit.png"
 const Hero=()=>{
     return(
@@ -11,7 +12,7 @@ const Hero=()=>{
 <h2 className="text-4xl font-bold ">I'm Ajit kumar</h2>
 <h2 className="text-xl  ">Web Developer & Designer</h2>
 <p>I'm a passionate web developer with expertise in React, Node.js, and modern web technologies. <br /> I love creating beautiful and functional website that solve real world problems.</p>
-<button className="w-36 p-2 bg-black text-white border-0" > <a href={resume} download='resume'>Download CV</a></button>
+<button className="w-36 p-2 bg-black text-white border-0" > <a href={resume1} download='resume'>Download CV</a></button>
         </div>
         <div className="md:w-1/2 relative flex justify-center items-end ml-20 ">
  <img src={ajit} alt=""  className="h-[90vh] mt-32 mr-14"/>
